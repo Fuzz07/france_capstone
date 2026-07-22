a8e77643cb558c523ac468dbb96f15a93c76528f
Fuzz07 <davealagban9tle@gmail.com>
2026-07-22 09:56:19 +0800

    Update landing.blade.php

--- resources/views/home/landing.blade.php
+++ resources/views/home/landing.blade.php
@@ -159,7 +159,7 @@
                     </div>
                     <div>
                         <div class="contact-label">Business Hours</div>
-                        <div class="contact-value">Mon – Sat, 8:00 AM – 6:00 PM</div>
+                        <div class="contact-value">Sun – Friday, 8:00 AM – 6:00 PM</div>
                     </div>
                 </div>
             </div>
